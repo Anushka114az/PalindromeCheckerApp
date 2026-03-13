3b5c1e680a5f0b58bd6f0f1be766f3bf85347191
Anushka114az <az5921@srmist.edu.in>
2026-03-14 00:16:10 +0530

    UC2: hardcodedpalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome palindrome Checker App");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Text: ");
+        String text = sc.next();
+        int n= text.length();
+        System.out.println(text);
+        int flag = 0;
+        for(int i=0; i<n; i++){
+            if ( text.charAt(i)!=text.charAt(n-1-i)){
+                flag =1;
+            }
+        }
+        if( flag ==0){
+            System.out.println("Is it a palindrome? : True");
+        }
+        else{
+            System.out.println("Is it a palindrome? : False");
+        }
     }
 }
